d28cc3ff44ede47b7dfeb8dc4a9c23520ec79e7d
Emmanouela Pothitou <emmanouelapothitou@gmail.com>
2020-10-05 18:37:16 +0300

    Updates

---
+++ precache-manifest.9a0bf20845d4a65bc502d69b807086a8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0108edb6ef40cc3ca1a05061001fc256",
+    "url": "/friends-quiz-app/index.html"
+  },
+  {
+    "revision": "2120d928956c98cb0e9e",
+    "url": "/friends-quiz-app/static/css/main.7080d938.chunk.css"
+  },
+  {
+    "revision": "6487e1045d380e20d63e",
+    "url": "/friends-quiz-app/static/js/2.0ea3ffaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friends-quiz-app/static/js/2.0ea3ffaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2120d928956c98cb0e9e",
+    "url": "/friends-quiz-app/static/js/main.d1840b47.chunk.js"
+  },
+  {
+    "revision": "96984aab6e5aa5e903ca",
+    "url": "/friends-quiz-app/static/js/runtime-main.82bde381.js"
+  },
+  {
+    "revision": "eacc5bfa331bb4803394bc5e910a0727",
+    "url": "/friends-quiz-app/static/media/cafe.eacc5bfa.png"
+  },
+  {
+    "revision": "07cecf681c02636dd87fed7437fad60f",
+    "url": "/friends-quiz-app/static/media/cat.07cecf68.png"
+  },
+  {
+    "revision": "fcd1acc3ebfe586c93b4d8b980ab1113",
+    "url": "/friends-quiz-app/static/media/chick.fcd1acc3.png"
+  },
+  {
+    "revision": "1c50ad4c5a8051e45b72d0268c6de911",
+    "url": "/friends-quiz-app/static/media/couch.1c50ad4c.png"
+  },
+  {
+    "revision": "9ebf938bb0a79444d0d7cf0a2b1a8700",
+    "url": "/friends-quiz-app/static/media/frame.9ebf938b.png"
+  },
+  {
+    "revision": "a3a97c21b5f760b9a5de7c4339898c04",
+    "url": "/friends-quiz-app/static/media/how.a3a97c21.png"
+  },
+  {
+    "revision": "911eacb38f7ee3eb3076955be1db9b86",
+    "url": "/friends-quiz-app/static/media/hugsy.911eacb3.png"
+  },
+  {
+    "revision": "d94e97631764cabe1972945447a8dd8a",
+    "url": "/friends-quiz-app/static/media/lobster.d94e9763.png"
+  },
+  {
+    "revision": "5b47baf3ce54c60d1d46e53e969f357b",
+    "url": "/friends-quiz-app/static/media/logo.5b47baf3.png"
+  },
+  {
+    "revision": "72c47f41bad012036dea122ae503d59a",
+    "url": "/friends-quiz-app/static/media/turkey.72c47f41.png"
+  }
+]);
